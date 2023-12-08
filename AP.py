cfe58246a0fcecd959ac9681248e9ab31e2fdfbe
AP-AIT <149565417+AP-AIT@users.noreply.github.com>
2023-12-08 11:36:35 +0530

    Update AP.py

--- AP.py
+++ AP.py
@@ -4,57 +4,66 @@ import email
 from datetime import datetime, timedelta
 import io
 from PIL import Image
-from google.cloud import vision_v1
-from google.cloud.vision_v1 import types
 
-def extract_text_from_image(image_bytes):
+def extract_text_from_email(msg):
+    text_parts = []
+    for part in msg.walk():
+        if part.get_content_type() == 'text/plain':
+            text_parts.append(part.get_payload(decode=True).decode(part.get_content_charset(), 'ignore'))
+    return '\n'.join(text_parts)
+
+def display_images_with_text(username, password, target_email, start_date):
+    image_and_text_data = []
+
     try:
-        client = vision_v1.ImageAnnotatorClient()
-        image = types.Image(content=image_bytes)
-        response = client.text_detection(image=image)
-        
-        # Extract the recognized text
-        annotations = response.text_annotations
-        if annotations:
-            extracted_text = annotations[0].description
-            return extracted_text
-        else:
-            return "No text found in the image."
-
-    except Exception as e:
-        return f"Text extraction error: {e}"
-
-def display_images(username, password, target_email, start_date):
-    try:
+        # Convert start_date to datetime object
         start_date = datetime.strptime(start_date, '%Y-%m-%d')
+
+        # Connect to the IMAP server (Gmail in this case)
         mail = imaplib.IMAP4_SSL('imap.gmail.com')
+
+        # Login to your email account
         mail.login(username, password)
+
+        # Select the mailbox (e.g., 'inbox')
         mail.select("inbox")
+
+        # Construct the search criterion using the date range and target email address
         search_criterion = f'(FROM "{target_email}" SINCE "{start_date.strftime("%d-%b-%Y")}" BEFORE "{(start_date + timedelta(days=1)).strftime("%d-%b-%Y")}")'
+
+        # Search for emails matching the criteria
         result, data = mail.uid('search', None, search_criterion)
         email_ids = data[0].split()
 
-        for idx, email_id in enumerate(email_ids, start=1):
+        # Iterate through the email IDs
+        for email_id in email_ids:
             result, msg_data = mail.uid('fetch', email_id, "(RFC822)")
             raw_email = msg_data[0][1]
+
+            # Parse the raw email content
             msg = email.message_from_bytes(raw_email)
 
+            # Extract text from the email
+            text_content = extract_text_from_email(msg)
+
+            # Iterate through email parts
             for part in msg.walk():
                 if part.get_content_maintype() == 'image':
-                    image_bytes = part.get_payload(decode=True)
-                    image = Image.open(io.BytesIO(image_bytes))
-                    st.image(image, caption=f'Image {idx}', use_column_width=True)
-
-                    extracted_text = extract_text_from_image(image_bytes)
-                    st.text(f'Text from Image {idx}: {extracted_text}')
-
+                    # Extract image data
+                    image_and_text_data.append({
+                        'image': part.get_payload(decode=True),
+                        'text': text_content
+                    })
     except Exception as e:
         st.error(f"An error occurred: {e}")
     finally:
+        # Logout from the IMAP server (even if an error occurs)
         mail.logout()
 
+    return image_and_text_data
+
 # Streamlit app
-st.title("Image Viewer")
+st.title("Image and Text Viewer")
 
 # Get user input through Streamlit
 email_address = st.text_input("Enter your email address:")
@@ -64,9 +73,20 @@ start_date = st.text_input("Enter the start date (YYYY-MM-DD):")
 
 # Check if the user has provided all necessary inputs
 if email_address and password and target_email and start_date:
-    # Display images when the user clicks the button
-    if st.button("View Images"):
-        # Display extracted images
-        display_images(email_address, password, target_email, start_date)
+    # Display images and text when the user clicks the button
+    if st.button("View Images and Text"):
+        # Display extracted images and text
+        data = display_images_with_text(email_address, password, target_email, start_date)
+
+        if not data:
+            st.warning("No images found.")
+
+        for idx, entry in enumerate(data, start=1):
+            # Display text content
+            st.text(f'Text {idx}: {entry["text"]}')
+
+            # Display image using PIL
+            image = Image.open(io.BytesIO(entry["image"]))
+            st.image(image, caption=f'Image {idx}', use_column_width=True)
 else:
     st.warning("Please fill in all the required fields.")
